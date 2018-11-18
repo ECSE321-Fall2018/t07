d230ad7d3b77586bb92e4ba23e81f5b000092159
norinmura <nswe.nrin@gmail.com>
2018-11-18 00:00:10 -0500

    @CrossOrigin added

--- src/main/java/com/ecse321/RideShare/controller/RideShareController.java
+++ src/main/java/com/ecse321/RideShare/controller/RideShareController.java
@@ -15,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
